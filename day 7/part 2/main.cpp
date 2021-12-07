70e36f7c0052d136faa21dada58460c6dac9a6e6
cathery <dumdumcathery@gmail.com>
2021-12-07 12:40:21 +0300

    Day 7

---
+++ day 7/part 2/main.cpp
@@ -0,0 +1,46 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <limits>
+
+int main()
+{
+	std::ifstream Input("input.txt");
+
+	std::vector<std::uintmax_t> CrabPositions;
+
+	std::uintmax_t Number;
+	while (Input >> Number)
+	{
+		CrabPositions.push_back(Number);
+		Input.ignore(1);
+	}
+
+	std::vector<std::uintmax_t> PathfindingMap;
+
+	for (const auto& CrabPosition : CrabPositions)
+	{
+		const std::uintmax_t NewSize = std::max(CrabPosition + 1, PathfindingMap.size());
+		PathfindingMap.resize(NewSize);
+
+		for (std::uintmax_t MapIndex = 0; MapIndex != NewSize; ++MapIndex)
+		{
+			const std::uintmax_t DistanceToPoint = MapIndex > CrabPosition ? MapIndex - CrabPosition : CrabPosition - MapIndex;
+
+			//Find the triangular number
+			PathfindingMap[MapIndex] += DistanceToPoint * (DistanceToPoint + 1) / 2;
+		}
+	}
+
+	std::uintmax_t LowestFuelCount = std::numeric_limits<std::uintmax_t>::max();
+	for (const auto& CurrentPositionFuelCount : PathfindingMap)
+	{
+		if (CurrentPositionFuelCount < LowestFuelCount)
+		{
+			LowestFuelCount = CurrentPositionFuelCount;
+		}
+	}
+
+
+	std::cout << LowestFuelCount << std::endl;
+}
